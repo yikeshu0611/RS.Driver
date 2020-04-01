7dd27ee787c658f25c776052078f26323898e532
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2020-04-01 22:44:17 +0800

    Add files via upload

---
+++ R/RS.clear_text.R
@@ -0,0 +1,11 @@
+#' Clear Text
+#'
+#' @param xpath xpath
+#'
+#' @return clear
+#' @export
+#'
+RS.clear_text <- function(xpath){
+    btn <- remDr$findElement(using='xpath',value=xpath)
+    btn$clearElement()
+}
---
+++ R/RS.locate.R
@@ -0,0 +1,11 @@
+#' Locate
+#'
+#' @param xpath xpath
+#'
+#' @return text
+#' @export
+#'
+RS.locate <- function(xpath){
+    read_html(remDr$getPageSource()[[1]][1]) %>%
+    html_nodes(xpath=xpath)
+}
